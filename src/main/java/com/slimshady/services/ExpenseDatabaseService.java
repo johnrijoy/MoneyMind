4db776731f9aa0ba24b120ee7915f454278518a7
Rijoy John <johnrijoy@gmail.com>
2022-07-17 10:54:03 +0530

    added ExpenseDatabaseService class for service related to Database manipulation
    the service class will offer services to manipulate database

---
+++ src/main/java/com/slimshady/services/ExpenseDatabaseService.java
@@ -0,0 +1,10 @@
+package com.slimshady.services;
+
+import java.util.Date;
+
+// CRUD expenses from database
+public class ExpenseDatabaseService {
+    public static void addExpense(float amount, String description, Date date) {
+
+    }
+}
